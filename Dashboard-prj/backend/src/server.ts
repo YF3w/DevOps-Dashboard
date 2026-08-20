35f42bdf17aa4e2859e2c5056313557582a24e59
YF3vv <120128975+YF3vv@users.noreply.github.com>
2026-08-20 21:01:16 +0200

    CPU + MEMORY + DISK VALUES ADDED

--- Dashboard-prj/backend/src/server.ts
+++ Dashboard-prj/backend/src/server.ts
@@ -1,5 +1,6 @@
 import express = require("express");
 import cors = require("cors");
+import lib = require("node-system-stats");
 
 const app = express();
 const PORT = 3000;
@@ -7,14 +8,96 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/metrics", (_req, res) => {
+
+const {usagePercent} = require("node-system-stats");
+//**************CPU metrics************************
+
+async function checkCpu(){
+
+  try {
+    const CPUused = await usagePercent();
+    
+    //console.log(`CPU Usage: ${CPUused.percent}%`);
+
+    return CPUused.percent;
+  } catch (err) {
+    console.error(err);
+  }
+  return;
+}
+checkCpu();
+//**********************FIN CPU metrics********************************
+
+
+//**************Memory metrics************************
+const { showMemoryUsage} = require("node-system-stats");
+async function checkMemory() {
+  try {
+    const Used = (await lib.getMemoryInfo());
+    /*
+    console.log("Memory info:", Used);
+    console.log(`Memory total: ${Used.total.gb}gb`);
+    console.log(`Memory free: ${Used.free.gb}gb`);
+    console.log(`Memory Used: ${Used.percentUsed}%`);
+  */
+
+    return { free: Used.free.gb, total: Used.total.gb, percent: Used.percentUsed };
+  } catch (err) {
+    console.error(err);
+  }
+  return;
+}
+checkMemory();
+
+//*********************FIN Memory metrics*********************************/
+
+
+
+
+//***********************Disk metrics*******************************/
+const { getDiskInfo, formatBytes } = require("node-system-stats");
+  async function checkDisks() {
+    try {
+
+    const Used = (await getDiskInfo("C:\\Users"));
+  /*
+    console.log("Disk info:", Used);
+    console.log(`Disk size: ${formatBytes(Used[0].size)}`);
+    console.log(`Disk used: ${formatBytes(Used[0].used)}`);
+    console.log(`Disk Used: ${Used[0].percentUsed}%`);
+     */
+
+    return Used[0].percentUsed;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+checkDisks();
+
+
+//***********************FIN Disk metrics*******************************
+
+
+
+
+//***********************API routes*******************************
+
+app.get("/api/metrics", async (_req, res) => {
   res.json({
-    cpu: 25,
-    memory: 48,
-    disk: 71,
+    "cpu (%)": await checkCpu(),
+    "memory": await checkMemory(),
+    "disk(%)": await checkDisks(),
   });
 });
+
 
 app.listen(PORT, () => {
   console.log(`API running on http://localhost:${PORT}`);
 });
+
+
+
+
+//class
+
